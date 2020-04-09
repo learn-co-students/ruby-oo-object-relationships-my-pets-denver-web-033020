b6ee1320d1b16cb15b3c4678dac08636e37fa0d1
LisaHJung <lisahjung@gmail.com>
2020-04-08 19:08:24 -0600

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,1 +1,15 @@
 Dir[File.join(File.dirname(__FILE__), "../lib", "*.rb")].each {|f| require f}
+
+
+=begin
+#lisa = Owner.new("Lisa")
+#wayne = Owner.new("Wayne")
+
+pluto = Dog.new("pluto", wayne)
+finn = Dog.new("finn", lisa)
+
+callie = Cat.new("Callie", "Wayne")
+chuckles = Cat.new("Chuckles", "Wayne")
+=end
+#binding.pry
+0
